ab3ca87761cdb97bb77583979b9259d266a50214
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-07-05 13:07:12 -0700

    Added required functions

--- calculator.js
+++ calculator.js
@@ -1,3 +1,27 @@
 const addValue = (value) => {
-  document.getElementById('screen').value = value;
+  document.getElementById('screen').value += value;
+}
+
+const clearValue = () => {
+  let screenInput = document.getElementById('screen');
+  let parent = screenInput.parentNode;
+  screenInput.value = ''; 
+  parent.innerHTML = '';
+  parent.appendChild(screenInput);
+}
+
+const processData = () => {
+  const screenInput = document.getElementById('screen');
+  const node = document.createElement("span");
+  node.classList.add("text-end")
+  const textnode = document.createTextNode(screenInput.value);
+  node.appendChild(textnode);
+  screenInput.parentNode.insertBefore(node, screenInput);
+  document.getElementById('screen').value = eval(screenInput.value);
+
+}
+
+const removeLast = () => {
+  let value = document.getElementById('screen').value;
+  document.getElementById('screen').value = value.slice(0, value.length-1)
 }
