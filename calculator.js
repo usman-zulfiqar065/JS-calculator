39bbe2819b830c0e6e9504ed4326f987783c51b9
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-07-05 13:15:48 -0700

    Added try/catch block for exception handling

--- calculator.js
+++ calculator.js
@@ -14,11 +14,17 @@ const processData = () => {
   const screenInput = document.getElementById('screen');
   const node = document.createElement("span");
   node.classList.add("text-end")
-  const textnode = document.createTextNode(screenInput.value);
-  node.appendChild(textnode);
+  try {
+    const textnode = document.createTextNode(screenInput.value);
+    document.getElementById('screen').value = eval(screenInput.value);
+    node.appendChild(textnode);
+  }
+  catch(err) {
+    const textnode = document.createTextNode(err.message);
+    node.appendChild(textnode);
+    node.classList.add("text-danger")
+  }
   screenInput.parentNode.insertBefore(node, screenInput);
-  document.getElementById('screen').value = eval(screenInput.value);
-
 }
 
 const removeLast = () => {
